8813b163c737ceee4d01b9f161263b65f62d9546
Paul Friedman <paulfri@hey.com>
2022-06-01 15:18:09 -0700

    Update with duration and pwd

--- README.md
+++ README.md
@@ -2,7 +2,10 @@
 
 <img width="710" alt="Screen Shot 2022-06-01 at 8 18 31 AM" src="https://user-images.githubusercontent.com/1364469/171439860-542e4fbe-d75d-4454-adf7-8ccb90941509.png">
 
-The `zsh` prompt I've been using for ages, but made into a zim module.
-Requires [zim](https://github.com/zimfw/zimfw) and
-[git-info](https://github.com/zimfw/git-info). Enable `viins` in `zle` for a
-special surprise.
+The `zsh` prompt I've been using for ages, but made into a zim module. Requires
+[zim](https://github.com/zimfw/zimfw),
+[git-info](https://github.com/zimfw/git-info),
+[duration-info](https://github.com/zimfw/duration-info) and
+[prompt-pwd](https://github.com/zimfw/prompt-pwd).
+
+Enable `viins` in `zle` for a special surprise.
--- init.zsh
+++ init.zsh
@@ -1,4 +1,5 @@
 autoload -U colors && colors
+autoload -Uz add-zsh-hook 
 setopt nopromptbang prompt{cr,percent,sp,subst}
 
 local cyan="%{$fg[cyan]%}"
@@ -7,29 +8,39 @@ local grey="%F{242}"
 local red="%{$fg[red]%}"
 local reset="%{$reset_color%}" 
 
+# git-info
 typeset -gA git_info
-if (( ${+functions[git-info]} )); then
-  zstyle ':zim:git-info' verbose yes
-  zstyle ':zim:git-info:branch' format '%b'
-  zstyle ':zim:git-info:commit' format '%c'
-  zstyle ':zim:git-info:clean' format '%F{green}λ'
-  zstyle ':zim:git-info:dirty' format '%F{red}λ'
-  zstyle ':zim:git-info:ahead' format '%F{green}↑%F{242}%A '
-  zstyle ':zim:git-info:behind' format '%F{red}↓%F{242}%B '
-  zstyle ':zim:git-info:keys' format \
-    'prompt' '%F{white}%C%D' \
-    'rprompt' ' %A%B%F{242}%b%F{white}'
+zstyle ':zim:git-info' verbose yes
+zstyle ':zim:git-info:branch' format '%b'
+zstyle ':zim:git-info:commit' format '%c'
+zstyle ':zim:git-info:clean' format '%F{green}λ'
+zstyle ':zim:git-info:dirty' format '%F{red}λ'
+zstyle ':zim:git-info:ahead' format '%F{green}↑%F{242}%A '
+zstyle ':zim:git-info:behind' format '%F{red}↓%F{242}%B '
+zstyle ':zim:git-info:keys' format \
+  'prompt' '%F{white}%C%D' \
+  'rprompt' ' %A%B%F{242}%b%F{white} '
 
-  autoload -Uz add-zsh-hook && add-zsh-hook precmd git-info
-fi
+add-zsh-hook precmd git-info
 
+# duration-info
+zstyle ':zim:duration-info' format '%d'
+zstyle ':zim:duration-info' threshold 2.0
+zstyle ':zim:duration-info' show-milliseconds yes
+
+add-zsh-hook preexec duration-info-preexec
+add-zsh-hook precmd duration-info-precmd
+
+# prompt-pwd
+# zstyle ':zim:prompt-pwd' git-root yes
+
+# prompt
 local user_host="%F{blue}%n@%m%{$reset_color%}"
-local working_dir="%F{242}%c%{$reset_color%}"
-local return_code="%(?..%F{red} %? ↵%{$reset_color%})"
+local return_code="%(?..%F{red} %?↵ %{$reset_color%})"
 local systime="%F{cyan}%*%{$reset_color%}"
 
-PS1='${user_host}:${working_dir} ${(e)git_info[prompt]:-λ} '
-local right_prompt='${return_code} ${(e)git_info[rprompt]} ${systime}'
+PS1='${user_host}:%F{242}$(prompt-pwd)%{$reset_color%} ${(e)git_info[prompt]:-λ} '
+local right_prompt='%F{242}${duration_info}%{$reset_color%}${return_code}${(e)git_info[rprompt]}${systime}'
 
 function zle-line-init zle-keymap-select {
   RPS1="${right_prompt} ${${KEYMAP/vicmd/☆}/(main|viins)/★}"
