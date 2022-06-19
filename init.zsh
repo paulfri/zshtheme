1fdbfb8fae63459195de05b1bbe497c3ddb3cac9
Paul Friedman <paulfri@hey.com>
2022-06-19 19:50:10 +0200

    Fix spacing bugs

--- init.zsh
+++ init.zsh
@@ -4,8 +4,8 @@ setopt nopromptbang prompt{cr,percent,sp,subst}
 
 local cyan="%{$fg[cyan]%}"
 local green="%{$fg[green]%}"
-local grey="%F{242}"
 local red="%{$fg[red]%}"
+local yellow="%{$fg[yellow]%}"
 local reset="%{$reset_color%}" 
 
 # git-info
@@ -19,12 +19,12 @@ zstyle ':zim:git-info:ahead' format '%F{green}↑%F{242}%A '
 zstyle ':zim:git-info:behind' format '%F{red}↓%F{242}%B '
 zstyle ':zim:git-info:keys' format \
   'prompt' '%F{white}%C%D' \
-  'rprompt' ' %A%B%F{242}%b%F{white} '
+  'rprompt' '%A%B%F{242}%b%F{white} '
 
 add-zsh-hook precmd git-info
 
 # duration-info
-zstyle ':zim:duration-info' format '%d'
+zstyle ':zim:duration-info' format '%F{yellow}%d '
 zstyle ':zim:duration-info' threshold 2.0
 zstyle ':zim:duration-info' show-milliseconds yes
 
@@ -32,15 +32,15 @@ add-zsh-hook preexec duration-info-preexec
 add-zsh-hook precmd duration-info-precmd
 
 # prompt-pwd
-# zstyle ':zim:prompt-pwd' git-root yes
+zstyle ':zim:prompt-pwd' git-root yes
 
 # prompt
 local user_host="%F{blue}%n@%m%{$reset_color%}"
-local return_code="%(?..%F{red} %?↵ %{$reset_color%})"
+local return_code="%(?..%F{red} %?↵ %{$reset_color%}) "
 local systime="%F{cyan}%*%{$reset_color%}"
 
-PS1='${user_host}:%F{242}$(prompt-pwd)%{$reset_color%} ${(e)git_info[prompt]:-λ} '
-local right_prompt='%F{242}${duration_info}%{$reset_color%}${return_code}${(e)git_info[rprompt]}${systime}'
+PS1='${user_host}:%F{242}$(prompt-pwd)%{$reset_color%} ${(e)git_info[prompt]:-λ}%{$reset_color%} '
+local right_prompt='${return_code}${duration_info}%{$reset_color%}${(e)git_info[rprompt]}${systime}%{$reset_color%}'
 
 function zle-line-init zle-keymap-select {
   RPS1="${right_prompt} ${${KEYMAP/vicmd/☆}/(main|viins)/★}"
